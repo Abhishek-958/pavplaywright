e75ed7a4242b3d62c81511375fedc5d328729812
Abhishek-958 <abhishek958k@gmail.com>
2026-04-17 23:36:38 +0530

    New changes done in pav code on 17th april

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -4,26 +4,26 @@ test("Handle checkboxes",async({page}) => {
     await page.goto('https://testautomationpractice.blogspot.com/');
     // single checkbox
     //await page.locator("//input[ @id='monday' and @type='checkbox'] ").check();
-    //await page.check("//input[ @id='monday' and @type='checkbox']");
+    await page.check("//input[ @id='monday' and @type='checkbox']");
 
-    //await expect(await page.locator("//input[ @id='monday' and @type='checkbox']")).toBeChecked();
-   // await expect(await page.locator("//input[ @id='monday' and @type='checkbox']").isChecked()).toBeTruthy();
-   // await expect(await page.locator("//input[ @id='sunday' and @type='checkbox']").isChecked()).toBeFalsy(); 
+   //await expect(await page.locator("//input[ @id='monday' and @type='checkbox']")).toBeChecked();
+    //await expect(await page.locator("//input[ @id='monday' and @type='checkbox']").isChecked()).toBeTruthy();
+    //await expect(await page.locator("//input[ @id='sunday' and @type='checkbox']").isChecked()).toBeFalsy(); 
     
     //Multiple checkboxes
-    const checkboxLocators =["//input[ @id='monday' and @type='checkbox']","//input[ @id='sunday' and @type='checkbox']","//input[ @id='saturday' and @type='checkbox']"];
+    //const checkboxLocators =["//input[ @id='monday' and @type='checkbox']","//input[ @id='sunday' and @type='checkbox']","//input[ @id='saturday' and @type='checkbox']"];
 
-    for(const locator of checkboxLocators)  //select multiple checkboxes
-    {
-        await page.locator(locator).check();
-    }
-    await page.waitForTimeout(5000);//pausing code
+   //for(const locator of checkboxLocators)  //select multiple checkboxes
+  //  {
+  //      await page.locator(locator).check();
+   // }
+   // await page.waitForTimeout(5000);//pausing code
 
-    for(const locator of checkboxLocators)  //unselect multiple checkboxes which are already selected
+   /* for(const locator of checkboxLocators)  //unselect multiple checkboxes which are already selected
     {   if (await page.locator(locator).isChecked()){
         
         await page.locator(locator).uncheck();} 
-    }
+    }*/
 
 
     await page.waitForTimeout(5000);//pausing code
